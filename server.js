7cdf39aa6f19b206b22a15ece378aaab893a11bb
niel98 <mosesdaniel201798@gmail.com>
2020-07-30 12:35:01 +0000

    routes added

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('API running!'))
 
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
