4cf92c33cb78fd0916f980fe7f9f99208831003b
niel98 <mosesdaniel201798@gmail.com>
2020-08-06 09:59:18 +0000

    getting user github repos added

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Post = mongoose.Schema('post', PostSchema) 
